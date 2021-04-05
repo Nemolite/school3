3785588e87c8d351f3d3c6ca9360df914a8cda23
Nemolite <g25092011@mail.ru>
2021-04-05 11:03:07 +0300

    Ссылка на сайт разрабочика

---
+++ inc/footer-link.php
@@ -0,0 +1,12 @@
+<?php
+/** 
+ * Информация и сслка на сайт разрабочика
+ */
+?>
+<div class="bottom-footer hmc-clearfix"><div class="hmc-container">	
+	<div class="site-info">	
+        <a href="http://vandraren.ru" target="_blank">
+            <p>Создание и техническая поддержка сайта: <span>VANDRAREN - Разработка web-проектов</span></p>
+        </a>
+    </div><!-- .site-info -->
+</div><!-- .hmc-container -->
