3edf359c2ecd678932aecafd1ad0d56e2466c8f3
Nemolite <g25092011@mail.ru>
2021-04-07 11:40:23 +0300

    Раздел объявления привязан к метополям

--- inc/block-announcement.php
+++ inc/block-announcement.php
@@ -3,33 +3,16 @@
  * Блок объявления
  */
 ?>
+<?php
+if ( 
+    (!empty ( ot_get_option( 'annonce_title' ) ) )&&    
+    (!empty ( ot_get_option( 'annonce_textarea' ) ) )  
+   ) {
+ ?>
 <div class="annouce-section">
     <div class="annouce-content">
-	<h2 class="section-title-doc"><span class="title-wrapper">Объявление</span></h2>
-    <p>Lorem ipsum dolor sit amet, 
-        consectetur adipisicing elit. 
-        Adipisci iste eligendi error 
-        reiciendis ipsa repudiandae 
-        quidem dolorem, similique 
-        sint qui incidunt expedita, 
-        enim quo? Officiis quas totam 
-        voluptatibus mollitia itaque 
-        nesciunt deserunt eveniet, 
-        dicta ab quaerat et illum 
-        voluptate molestias impedit 
-        cupiditate at eum provident 
-        in iure, ullam, libero odio? 
-        Iste, odit veritatis in neque 
-        obcaecati consequuntur enim 
-        nihil odio illo delectus corporis 
-        dolores sit quasi saepe optio 
-        unde ea recusandae exercitationem 
-        officiis quia rem? Qui modi 
-        corrupti vel id aperiam 
-        officiis in eligendi praesentium 
-        suscipit hic nisi neque veritatis 
-        nam laborum, blanditiis error 
-        voluptas delectus quasi! 
-        Tempora, voluptatem et.</p>
+	<h2 class="section-title-doc"><span class="title-wrapper"><?php echo ot_get_option( 'annonce_title' ); ?></span></h2>
+    <p><?php echo ot_get_option( 'annonce_textarea' ); ?></p>
     </div>
 </div>
+<?php } ?>
